e96b211f5039017da6498c92678dca949b568541
Atchuthan Dinesh <a.dinesh@hotmail.fr>
2022-08-23 21:46:15 +0200

    add event at zombie creation

--- Contract.sol
+++ Contract.sol
@@ -2,6 +2,15 @@ pragma solidity ^0.4.19;
 
 contract ZombieFactory{
     
+    // event: tell the front-end that something happened
+    event NewZombie(uint zombieId, string name, uint dna);
+    /*
+    javascript implementation example (front-end waiting for the event)
+    ZombieFactory.NewZombie(function(error, dna) {
+        // do something
+    }
+    */
+
     // state variables are stored permanently in the blockchain.
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
@@ -20,7 +29,9 @@ contract ZombieFactory{
     // if no state is modified, the function must be declared as "view"
     // if it uses no data from the app, the function must be "pure"
     function _createZombie(string _name, uint _dna) private {
-        zombies.push(Zombie(_name, _dna)); // add a zombie to the dynamic array
+        // array.push returns the new length of the array
+        uint id = zombies.push(Zombie(_name, _dna)) - 1; // add a zombie to the dynamic array
+        NewZombie(id, _name, _dna);
     }
 
     function _generateRandomDna(string _str) private view returns (uint) {
@@ -28,5 +39,10 @@ contract ZombieFactory{
         // used here to generate random numbers although it is not secure to do so.
         uint rand = uint(keccak256(_str));
         return rand % dnaModulus;
+    }
+
+    function createRandomZombie(string _name) public {
+        uint randDna = _generateRandomDna(_name);
+        _createZombie(_name, randDna);
     }
 }
