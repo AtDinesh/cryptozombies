c37972ad40915e8db5bbfa9c5c2af38bdd5cce24
Atchuthan Dinesh <a.dinesh@hotmail.fr>
2022-08-24 00:11:24 +0200

    special genes for feeding with kitty

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -22,7 +22,7 @@ contract ZombieFeeding is ZombieFactory {
     address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
     KittyInterface kittyContract = KittyInterface(ckAddress);
 
-    function feedAndMultiply(uint _zombieId, uint _targetDna) public {
+    function feedAndMultiply(uint _zombieId, uint _targetDna, string _species) public {
         // Make sure the user owns the zombie
         require(msg.sender == zombieToOwner[_zombieId]);
         Zombie storage myZombie = zombies[_zombieId];
@@ -30,6 +30,11 @@ contract ZombieFeeding is ZombieFactory {
         // make sure _targetDna only has 16 figures
         _targetDna = _targetDna % dnaModulus;
         uint newDna = (myZombie.dna + _targetDna) / 2;
+
+        // add bonus kitty genes
+        if (keccak256(_species) == keccak256("kitty")){
+        newDna = newDna - newDna % 100 + 99;
+        }
         _createZombie("NoName", newDna);
     }
 
@@ -38,6 +43,6 @@ contract ZombieFeeding is ZombieFactory {
         // only interested in the Dna
         uint kittyDna;
         (,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId);
-        feedAndMultiply(_zombieId, kittyDna);
+        feedAndMultiply(_zombieId, kittyDna, "kitty");
     }
 }
