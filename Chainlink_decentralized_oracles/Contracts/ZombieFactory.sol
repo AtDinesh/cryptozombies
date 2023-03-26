be5ba65ec9bcac4143e6f64ede86505b50f2ef3c
Atchuthan Dinesh <a.dinesh@hotmail.fr>
2023-03-26 10:25:19 +0200

    Chapter 10: requestRandomness & fulfillRandomness

--- Chainlink_decentralized_oracles/Contracts/ZombieFactory.sol
+++ Chainlink_decentralized_oracles/Contracts/ZombieFactory.sol
@@ -6,7 +6,6 @@ contract ZombieFactory is VRFConsumerBase {
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
 
-    // 1. Define the `keyHash`, `fee`, and `randomResult` variables. Don't forget to make them `public`.
     bytes32 public keyHash;
     uint256 public fee;
     uint256 public randomResult;
@@ -22,13 +21,23 @@ contract ZombieFactory is VRFConsumerBase {
         0x6168499c0cFfCaCD319c818142124B7A15E857ab, // VRF Coordinator
         0x01BE23585060835E02B77ef475b0Cc51aA1e0709  // LINK Token
     ) public{
-      // 2. Fill in the body
-      keyHash = 0x2ed0feb3e7fd2022120aa84fab1945545a9f2ffc9076fd6156fa96eaff4c1311;
-      fee = 100000000000000000;
+        keyHash = 0x2ed0feb3e7fd2022120aa84fab1945545a9f2ffc9076fd6156fa96eaff4c1311;
+        fee = 100000000000000000;
+
     }
 
     function _createZombie(string memory _name, uint _dna) private {
         zombies.push(Zombie(_name, _dna));
+    }
+
+    // 1. Create the `getRandomNumber` function
+    function getRandomNumber() public returns (bytes32 requestId) {
+        return requestRandomness(keyHash, fee);
+    }
+    // 2. Create the `fulfillRandomness` function
+    // Made internal override because only the VRF Coordinator contract calls this function.
+    function fulfillRandomness(bytes32 requestId, uint256 randomness) internal override {
+        randomResult = randomness;
     }
 
     function _generatePseudoRandomDna(string memory _str) private view returns (uint) {
